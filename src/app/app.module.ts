e58d83e673b0c6a25af38fc55c8382d7d2cf533b
BachNguyen2001 <Bachnguyenfptu@gmail.com>
2022-04-20 20:51:15 +0700

    add chart component and chart services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChartComponent } from './components/chart/chart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChartComponent
   ],
   imports: [
     BrowserModule,
