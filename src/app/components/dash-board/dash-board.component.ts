9fbf4f26c94f766def97d478327bee008871e3d3
Hoàng Hiếu <55184437+solochithanh@users.noreply.github.com>
2022-04-21 12:19:34 +0700

    Update dash-board.component.ts

--- src/app/components/dash-board/dash-board.component.ts
+++ src/app/components/dash-board/dash-board.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { RegionServicesService } from 'src/app/services/region-services.service';
+
 
 @Component({
   selector: 'app-dash-board',
   templateUrl: './dash-board.component.html',
-  styleUrls: ['./dash-board.component.css']
+  styleUrls: ['./dash-board.component.css'],
 })
 export class DashBoardComponent implements OnInit {
-
-  constructor() { }
+  showOrHide=true
+  constructor(private regionSer: RegionServicesService) {}
 
   ngOnInit(): void {
+ 
   }
-
+  toggleShow(){
+    this.showOrHide=!this.showOrHide
+  }
+ 
 }
